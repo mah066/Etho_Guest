beb40ccbbdcca999d732ebd083b60d24be95bece
mah066 <muhammedaazimhasher@gmail.com>
2026-06-19 22:02:14 +0100

    Changed the logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
 import { Button } from '../ui/Button';
 import { Container } from './Container';
-// import logo from '../../assets/EthoGuestLogo.jpeg';
+import logo from '../../assets/EthoGuestLogo.png';
 
 export function Navbar() {
     const [isScrolled, setIsScrolled] = useState(false);
@@ -30,11 +30,11 @@ export function Navbar() {
                 <div className="flex items-center justify-between">
                     <div className="flex-shrink-0">
                         <a href="#" className="flex items-center gap-2 text-2xl font-bold text-white tracking-wider">
-                             {/* <img
+                             <img
                                 src={logo}
                                 alt="EthoGuest logo"
                                 className="h-20 w-30 object-contain"
-                                /> */}
+                                />
 
                         <span
   className="text-3xl font-semibold tracking-wide"
